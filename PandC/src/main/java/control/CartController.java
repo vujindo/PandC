31529eab0d7b8bd22d334d01bbdd611b6d5cec01
le anh vu <username>
2022-03-07 19:58:41 +0900

    show cart

---
+++ PandC/src/main/java/control/CartController.java
@@ -0,0 +1,58 @@
+package control;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.CartDao;
+import model.cart;
+
+/**
+ * Servlet implementation class CartController
+ */
+@WebServlet("/CartController")
+public class CartController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String goodID = request.getParameter("goodID");
+		String action = request.getParameter("action");
+		String path = "";
+		
+		HttpSession session = request.getSession();
+		CartDao cd = new CartDao();
+		
+		if (action.equals("1")) {
+			cd.addCart(goodID);
+			path = "views/cart/cart2.jsp";
+			session.setAttribute("cart_gname", request.getParameter("goodName"));
+			session.setAttribute("cart_gprice", request.getParameter("price"));
+		}else if(action.equals("0")) {
+			ArrayList<cart> clist = cd.findCart();
+			if (clist.isEmpty()) {
+				path = "views/cart/cart.jsp";
+			}else {
+				session.setAttribute("cartlist", clist);
+				path = "views/cart/cart_product.jsp";
+			}
+			
+		}
+		
+		response.sendRedirect(path);
+	}
+	
+}
---
+++ PandC/src/main/java/dao/CartDao.java
@@ -0,0 +1,72 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import model.cart;
+
+public class CartDao {
+	private Connection con = null;
+
+	// オブジェクト生成時に DB に接続
+	public CartDao() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://10.42.129.142:3306/"
+					+ "20gr21?characterEncoding=UTF-8", "20gr21", "20gr21");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public void connectionClose() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void addCart(String id) {
+		String sql = "INSERT INTO cart(goodsID,amount) VALUES (?,1)";
+		try {
+			PreparedStatement state = con.prepareStatement(sql);
+			state.setString(1, id);
+			state.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<cart> findCart() {
+		String sql = "SELECT * FROM cart INNER JOIN goods ON cart.goodsID = goods.goodsID";
+		ArrayList<cart> ary = null;
+		try {
+			Statement state = con.createStatement();
+			ResultSet rs = state.executeQuery(sql);
+			ary = new ArrayList<cart>();
+			while (rs.next()) {
+				cart one = new cart();
+				one.setGoodsName(rs.getString("goodsName"));
+				one.setGoodsImg(rs.getString("goodsImg"));
+				one.setPrice(rs.getString("goodsPrice"));
+				one.setValue(rs.getString("goodsValue"));
+				ary.add(one);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ary;
+	}
+}
